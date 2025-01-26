317148a50aca259a2a89e878935609af45210fd9
xuxin <xuxin@espressif.com>
2025-01-26 14:58:25 +0800

    feat: esp-sparkbot worked

--- src/webrtc.cpp
+++ src/webrtc.cpp
@@ -59,10 +59,19 @@ static void oai_onconnectionstatechange_task(PeerConnectionState state,
 #endif
   } else if (state == PEER_CONNECTION_CONNECTED) {
 #ifndef LINUX_BUILD
+#if CONFIG_OPENAI_BOARD_ESP32_S3
     StackType_t *stack_memory = (StackType_t *)heap_caps_malloc(
         20000 * sizeof(StackType_t), MALLOC_CAP_SPIRAM);
     xTaskCreateStaticPinnedToCore(oai_send_audio_task, "audio_publisher", 20000,
                                   NULL, 7, stack_memory, &task_buffer, 0);
+#elif CONFIG_OPENAI_BOARD_SPARK_BOT
+    // Because we change the sampling rate to 16K, so we need increased the
+    // memory size, if not will overflow :)
+    StackType_t *stack_memory = (StackType_t *)heap_caps_malloc(
+        40000 * sizeof(StackType_t), MALLOC_CAP_SPIRAM);
+    xTaskCreateStaticPinnedToCore(oai_send_audio_task, "audio_publisher", 40000,
+                                  NULL, 7, stack_memory, &task_buffer, 0);
+#endif
 #endif
   }
 }
@@ -71,6 +80,13 @@ static void oai_on_icecandidate_task(char *description, void *user_data) {
   char local_buffer[MAX_HTTP_OUTPUT_BUFFER + 1] = {0};
   oai_http_request(description, local_buffer);
   peer_connection_set_remote_description(peer_connection, local_buffer);
+}
+
+static void peer_loop_task(void *arg) {
+  while (1) {
+    peer_connection_loop(peer_connection);
+    vTaskDelay(pdMS_TO_TICKS(TICK_INTERVAL));
+  }
 }
 
 void oai_webrtc() {
@@ -106,8 +122,6 @@ void oai_webrtc() {
 
   peer_connection_create_offer(peer_connection);
 
-  while (1) {
-    peer_connection_loop(peer_connection);
-    vTaskDelay(pdMS_TO_TICKS(TICK_INTERVAL));
-  }
+  xTaskCreatePinnedToCore(&peer_loop_task, "peer connect task", 16 * 1024, NULL,
+                          6, NULL, 0);
 }
